2bffe87d22427ad091f08900a99f085a0272d143
Geon <114966864+geon0430@users.noreply.github.com>
2024-02-08 11:27:31 +0900

    Update install_ffmpeg.sh

--- setting_scripts/install_ffmpeg.sh
+++ setting_scripts/install_ffmpeg.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-cd / && \
-    mkdir -p /ffmpeg && cd /ffmpeg && \
-    git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git && \
-        cd nv-codec-headers && \
-        git checkout sdk/12.0 && \
-        make -j 8 && \
-        make install && \
-        ldconfig && \
-        cd - && \
-    git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg/ && \
-		cd ffmpeg && \
-        apt-get update && \
-	    apt-get install -y libx264-dev libx265-dev && \
-		./configure --enable-nonfree --enable-ffnvcodec --enable-cuda-nvcc --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 --disable-static --enable-shared && \
-        make -j 8 && \
-        make install && \
-        ldconfig
 
-# rm -r /ffmpeg
+cd / && mkdir -p /ffmpeg && cd /ffmpeg
+
+git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+cd nv-codec-headers
+git checkout sdk/12.0
+make -j 8 && make install
+ldconfig
+cd ..
+
+git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg/
+cd ffmpeg/
+
+
+apt-get update
+apt-get install -y software-properties-common
+add-apt-repository multiverse
+apt-get update
+
+
+apt-get install -y libx264-dev libx265-dev
+
+
+./configure --enable-nonfree --enable-gpl --enable-libx264 --enable-ffnvcodec --enable-cuda-nvcc --enable-libnpp \
+--extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 \
+--disable-static --enable-shared
+
+make -j 8 && make install
+ldconfig
+
+
+cd / && rm -rf /ffmpeg
